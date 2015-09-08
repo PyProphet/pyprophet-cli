5162d14ccd641cfcaeef0651bd0b0b9842d361f9
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2015-09-08 11:46:58 +0200

    SSDM-2425 : added missing file from commit 841661b54d6dc9fbfdee97cbdeaa82d8c302bb24

---
+++ pyprophet_cli/version.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+from __future__ import print_function
+
+
+import pkg_resources  # part of setuptools
+version = tuple(map(int, pkg_resources.require("pyprophet-cli")[0].version.split(".")))
